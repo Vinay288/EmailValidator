886129ebae3bb9f584d21dccf0e4909eadb4e0b2
Vinay288 <vinayhiremath288@gmail.com>
2021-10-08 01:11:00 +0530

    MODIFIED: validating with given input

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,1 +1,17 @@
 const emailRegex = RegExp("^[abc][A-Za-z0-9]+[@]{1}[bridgelabz]+[.][co][.][a-zA-Z]+$");
+function validateEmail(email)
+{
+    if(emailRegex.test(email))
+        console.log(email + " is Valid");
+    else
+        throw new Error(email+"inavlid email")
+}
+try
+{
+    validateEmail("abca.abc@gmail.com");
+    validateEmail(".abc@abc.com");
+}
+catch(e)
+{
+    console.error(e);
+}
